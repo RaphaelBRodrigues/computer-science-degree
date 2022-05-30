9f0710b67189c1c9fb0b1632b4721a91b1623493
RaphaelBRodrigues <raphaelbarbosa.rodrigues@gmail.com>
2022-05-29 21:35:39 -0300

    pl: adding comments

--- quinto_semestre/PL/event_driven/Event.js
+++ quinto_semestre/PL/event_driven/Event.js
@@ -6,12 +6,15 @@ class EventExemple {
   constructor() {
     this.eventEmitter = new events.EventEmitter()
   }
+
+  // Método responsável por emitir eventos 
   dispatch() {
     this.eventEmitter.emit('exemple', {
       success: true,
     })
   }
 
+  // Método responsável por adicionar listeners 
   addEventListener(eventName, callback) {
     return this.eventEmitter.on(eventName, callback)
   }
--- quinto_semestre/PL/event_driven/index.js
+++ quinto_semestre/PL/event_driven/index.js
@@ -2,10 +2,12 @@ const EventExemple = require('./Event')
 
 const exemple = new EventExemple()
 
+// Listener responsável por tratar o evento
 exemple.addEventListener('exemple', (event) => {
   console.log(event)
 })
 
 setInterval(() => {
+  // Método do objeto exemple responsávelo por disparar o evento
   exemple.dispatch()
 }, 3000)
